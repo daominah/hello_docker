57d15c63bd4da9a1beff9f98de4fc31ef330409f
Đào Thanh Tùng <tung.dao@techx.vn>
2020-06-12 17:36:58 +0700

    wip mysql group 2

--- mysql_group_replication/start_containers.sh
+++ mysql_group_replication/start_containers.sh
@@ -1,7 +1,7 @@
 for N in 11 12 13
 do docker run -d \
     --name=node$N --net=mysql_group \
-    --hostname=node$N --ip=172.19.0.$N\
+    --hostname=node$N --ip=172.19.0.$N \
     -v $PWD/etc_mysql_conf.d_$N:/etc/mysql/conf.d \
     -v $PWD/var_lib_mysql_$N:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=123qwe \
