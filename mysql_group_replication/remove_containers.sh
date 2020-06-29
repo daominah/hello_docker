d649ade4f6245c606fe8f79b9f408fdf5cefd10b
Đào Thanh Tùng <tung.dao@techx.vn>
2020-06-29 11:29:51 +0700

    using envsubst for config

--- mysql_group_replication/remove_containers.sh
+++ mysql_group_replication/remove_containers.sh
@@ -1,6 +1,10 @@
-for N in 11 12 13
+export SERVER_IDS=(101 102 103)
+export NAME_PRE=mysqlGroup
+
+for SERVER_ID in ${SERVER_IDS[@]}
 do
-    docker stop -t 0 node$N
-    docker rm node$N
-    sudo rm -rf ./var_lib_mysql_$N
+    docker stop -t 1 $NAME_PRE$SERVER_ID
+    docker rm $NAME_PRE$SERVER_ID
+    rm -rf etc_mysql_conf.d_$SERVER_ID
+    sudo rm -rf var_lib_mysql_$SERVER_ID
 done
--- mysql_group_replication/start_containers.sh
+++ mysql_group_replication/start_containers.sh
@@ -1,9 +1,28 @@
-for N in 11 12 13
-do docker run -d \
-    --name=node$N --net=mysql_group \
-    --hostname=node$N --ip=172.19.0.$N \
-    -v $PWD/etc_mysql_conf.d_$N:/etc/mysql/conf.d \
-    -v $PWD/var_lib_mysql_$N:/var/lib/mysql \
-    -e MYSQL_ROOT_PASSWORD=123qwe \
-  mysql:5.7
+export SERVER_IDS=(101 102 103)
+export NAME_PRE=mysqlGroup
+
+export HOSTS=()
+for ID in ${SERVER_IDS[@]}
+    do export HOSTS+=(172.19.0.$ID)
+done
+export GROUP_SEED="${HOSTS[0]}:33061"
+
+
+for i in ${!SERVER_IDS[@]}
+do
+    export SERVER_ID=${SERVER_IDS[i]}
+    export HOST=${HOSTS[i]}
+    CONFIG_DIR=$PWD/etc_mysql_conf.d_$SERVER_ID
+    DATA_DIR=$PWD/var_lib_mysql_$SERVER_ID
+    mkdir -p $CONFIG_DIR
+    envsubst < "./etc_mysql_conf.d/config-file.cnf" \
+        > $CONFIG_DIR/config-file.cnf
+
+    docker run -d --name=$NAME_PRE$SERVER_ID --net=mysql_group --ip=$HOST \
+         -v $CONFIG_DIR:/etc/mysql/conf.d -v $DATA_DIR:/var/lib/mysql \
+         -e MYSQL_ROOT_PASSWORD=123qwe \
+         mysql:8.0.20
+    # Warning: Creating a group and joining multiple members
+    # at the same time is not supported
+    sleep 2
 done
