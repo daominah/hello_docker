5853ff99c44a168a6f9a262568dc8b4348b73cc1
Đào Thanh Tùng <tung.dao@techx.vn>
2020-07-23 17:29:56 +0700

    change portainer port

--- hello_swarm/sy_install_portainer.sh
+++ hello_swarm/sy_install_portainer.sh
@@ -1,11 +1,11 @@
 docker pull portainer/portainer
 docker volume create portainer_data
-docker run -d -p 9000:9000 \
+docker run -d -p 48036:9000 \
     --name=dockerui_portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
     portainer/portainer
 
-# A docker UI should be available on 127.0.0.1:9000
+# A docker UI should be available on 127.0.0.1:48036
 # Connect to remote docker hosts with ${HOME}/.docker/machine/certs
