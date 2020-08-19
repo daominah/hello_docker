6c8dd1b97e2282829582189d066b37a353cc0c93
Đào Thanh Tùng <tung.dao@techx.vn>
2020-08-19 17:36:29 +0700

    fix entrypoint when etcd password empty

--- etcd/image/entrypoint.sh
+++ etcd/image/entrypoint.sh
@@ -5,18 +5,22 @@ if [[ $(stat -c "%a" "$ETCD_DATA_DIR") != *700 ]]; then
     chmod -R 700 "$ETCD_DATA_DIR"
 fi
 
-(
-    sleep $(( 5 + $RANDOM % 5))
-    etcdUsers=$(etcdctl user list 2>&1)
-    if [ -z "${etcdUsers}" ]; then
-        echo "$(date --iso=ns): user list is empty, will create user root"
-        echo "$ETCD_ROOT_PASSWORD" | etcdctl user add root --interactive=false
-        etcdctl user grant-role root root
-        etcdctl auth enable
-        echo "$(date --iso=ns): done etcdctl auth enable"
-    else
-        echo "$(date --iso=ns): return of user list: ${etcdUsers}"
-    fi
-) &
+if [ -n "$ETCD_ROOT_PASSWORD" ]; then
+    (
+        sleep $(( 5 + $RANDOM % 5))
+        etcdUsers=$(etcdctl user list 2>&1)
+        if [ -z "${etcdUsers}" ]; then
+            echo "$(date --iso=ns): user list is empty, will create user root"
+            echo "$ETCD_ROOT_PASSWORD" | etcdctl user add root --interactive=false
+            etcdctl user grant-role root root
+            etcdctl auth enable
+            echo "$(date --iso=ns): done etcdctl auth enable"
+        else
+            echo "$(date --iso=ns): return of user list: ${etcdUsers}"
+        fi
+    ) &
+else
+    echo "warn: any client can access etcd cluster without auth"
+fi
 
 exec "$@"
