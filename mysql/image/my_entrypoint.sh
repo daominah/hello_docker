2fdc71103329c5c45bb29d7c0c68856d10cf1705
Đào Thanh Tùng <tung.dao@techx.vn>
2021-05-27 13:11:53 +0700

    change config MYSQL_IS_GROUP_REPLICATION must be TRUE to select this mode

--- mysql/image/my_entrypoint.sh
+++ mysql/image/my_entrypoint.sh
@@ -3,10 +3,8 @@
 set -e
 
 
-if [[ "${MYSQL_IS_GROUP_REPLICATION^^}" == "" ]]; then
+if [[ "${MYSQL_IS_GROUP_REPLICATION^^}" == "TRUE" ]]; then
     echo "generate mysqld config file from cnf_normal_tpl"
-    envsubst < "/config-file.cnf_normal_tpl" > "/etc/mysql/conf.d/config-file.cnf"
-else
     if [[ -e /is_2nd_boot ]]; then
         echo "generate mysqld config file from cnf_rep_group_tpl (2nd boot)"
         export MYSQL_GROUP_REPLICATION_START_ON_BOOT=ON
@@ -14,6 +12,9 @@ else
         echo "generate mysqld config file from cnf_rep_group_tpl (1st boot)"
     fi
     envsubst < "/config-file.cnf_rep_group_tpl" > "/etc/mysql/conf.d/config-file.cnf"
+else
+    envsubst < "/config-file.cnf_normal_tpl" > "/etc/mysql/conf.d/config-file.cnf"
+
 fi
 echo "cat /etc/mysql/conf.d/config-file.cnf:"
 cat /etc/mysql/conf.d/config-file.cnf
