ebd436056a6ad925a83e337ebff457c7e4a31d9b
Đào Thanh Tùng <tung.dao@difisoft.com>
2023-09-10 15:32:00 +0700

    update to go1.21.1, WIP http_server_sys_load

--- go121/s0_build_img.sh
+++ go121/s0_build_img.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
-
+export dockerImgTag=daominah/go121
 # option: `--progress=plain`, `--no-cache`
-docker build  --tag=daominah/go121 .
+docker build --tag=$dockerImgTag .
+export isBuiltWell=$?
+if [[ $isBuiltWell -eq 0 ]]; then
+    echo "built image $dockerImgTag with cache"
+else
+    echo "error when build image, try to build --no-cache"
+    docker build --tag=$dockerImgTag --no-cache .
+    export isBuiltWell=$?
+fi
 
-docker push daominah/go121
-
-set +e
+if [[ $isBuiltWell -eq 0 ]]; then
+    echo "successfully built, pushing it to to hub.docker.com"
+    # docker push $dockerImgTag
+else
+    echo "fail to build image $dockerImgTag"
+fi
--- go121_lean_example/go.mod
+++ go121_lean_example/go.mod
@@ -2,4 +2,4 @@ module github.com/daominah/hello_docker/go121_lean_example
 
 go 1.21.0
 
-require github.com/julienschmidt/httprouter v1.3.0
+require github.com/julienschmidt/httprouter v1.3.1-0.20220603155159-34250257ea14
